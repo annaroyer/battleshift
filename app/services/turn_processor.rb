ea6f9e7473066288e4ad695e3c66e6738202cd73
amj133 <amj@vt.edu>
2018-03-29 15:46:40 -0600

    add game_status and game_over to return game over if a player sinks all opponent ships

--- app/services/turn_processor.rb
+++ app/services/turn_processor.rb
@@ -13,7 +13,7 @@ class TurnProcessor
   def run!
     begin
       attack_opponent
-      @messages << "Game over." if game_over?
+      game_status
       game.save!
     rescue InvalidAttack => e
       @messages << e.message
@@ -25,40 +25,48 @@ class TurnProcessor
     @messages.join(" ")
   end
 
-  def game_over?
-    opponent_board.board.flatten.all? do |space|
-      space.values.first.contents.nil? || space.values.first.contents.is_sunk?
-    end
-  end
+
 
   private
+    attr_reader :game, :target, :opponent_board, :player
 
-  attr_reader :game, :target, :opponent_board, :player
+    def game_status
+      if game_over?
+        @messages << "Game over."
+        game.winner = User.find_by(api_key: player.api_key).email
+      end
+    end
 
-  def attack_opponent
-    result = Shooter.fire!(board: opponent_board, target: target)
-    @messages << "Your shot resulted in a #{result}."
-    switch_turns
-  end
+    def game_over?
+      opponent_board.board.flatten.all? do |space|
+        space.values.first.contents.nil? || space.values.first.contents.is_sunk?
+      end
+    end
 
-  def switch_turns
-    player.turns += 1
-    game.current_turn = game.player_1.turns - game.player_2.turns
-    game.save!
-  end
+    def attack_opponent
+      result = Shooter.fire!(board: opponent_board, target: target)
+      @messages << "Your shot resulted in a #{result}."
+      switch_turns
+    end
 
-  # def ai_attack_back
-  #   result = AiSpaceSelector.new(player.board).fire!
-  #   @messages << "The computer's shot resulted in a #{result}."
-  #   game.player_2_turns += 1
-  # end
+    def switch_turns
+      player.turns += 1
+      game.current_turn = game.player_1.turns - game.player_2.turns
+      game.save!
+    end
 
-  # def player
-  #   Player.new(game.player_1_board)
-  # end
-  #
-  # def opponent
-  #   Player.new(game.player_2_board)
-  # end
+    # def ai_attack_back
+    #   result = AiSpaceSelector.new(player.board).fire!
+    #   @messages << "The computer's shot resulted in a #{result}."
+    #   game.player_2_turns += 1
+    # end
+
+    # def player
+    #   Player.new(game.player_1_board)
+    # end
+    #
+    # def opponent
+    #   Player.new(game.player_2_board)
+    # end
 
 end
