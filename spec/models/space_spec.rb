6ca1aeaeab9a1872aa482dfb8c6a13962ee30eb3
Anna Royer <anna.royer@colorado.edu>
2018-03-30 17:55:23 -0600

    add test for attack! method on space

--- spec/models/space_spec.rb
+++ spec/models/space_spec.rb
@@ -24,4 +24,27 @@ describe Space do
       expect(space.status).to eq('Not Attacked')
     end
   end
+
+  context 'instance methods' do
+    describe '#attack!' do
+      it "attacks it's contents and returns whether it was a hit or miss" do
+        ship = spy("ship")
+        space = Space.new('A1')
+        allow(space).to receive(:contents).and_return(ship)
+
+        with_a_ship = space.attack!
+
+        expect(ship).to have_received(:attack!)
+        expect(with_a_ship).to eq('Hit')
+        expect(space.status).to eq('Hit')
+
+        space_2 = Space.new('A2')
+
+        without_a_ship = space_2.attack!
+
+        expect(without_a_ship).to eq('Miss')
+        expect(space_2.status).to eq('Miss')
+      end
+    end
+  end
 end
