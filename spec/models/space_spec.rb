fed2cb03fb6439f1b75bd1de19cfe25386590f40
Anna Royer <anna.royer@colorado.edu>
2018-03-30 17:20:03 -0600

    add attribute tests

--- spec/models/space_spec.rb
+++ spec/models/space_spec.rb
@@ -10,8 +10,18 @@ describe Space do
 
     it 'has contents' do
       ship = Ship.new(2)
-      ShipPlacer.new()
+      board = Board.new
+      ship_placer = ShipPlacer.new({board: board, ship: ship, start_space: 'A1', end_space: 'A2'})
+      ship_placer.run
+      space = board.locate_space('A1')
+
+      expect(space.contents.class).to eq(Ship)
+    end
+
+    it "starts with status 'Not Attacked'" do
       space = Space.new('A1')
+
+      expect(space.status).to eq('Not Attacked')
     end
   end
 end
