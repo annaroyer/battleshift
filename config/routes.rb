981c6d5dc290572aa206e43fa1fc84344fcbdf61
Anna Royer <anna.royer@colorado.edu>
2018-03-31 14:19:33 -0600

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,8 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :games, only: [:show, :create], module: :games do
+      resources :games, only: [:show, :create]
+      namespace :games do
         resources :shots, only: [:create]
         resources :ships, only: [:create]
       end
