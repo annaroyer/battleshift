821ad7f6f49037c27e9b847ffa02cd05e6a54f92
amj133 <amj@vt.edu>
2018-03-31 17:11:26 -0600

    update game spec to include validations and test block for current_turn

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:player_1) { "challenger" }
+  let(:player_2) { "opponent" }
+  subject {
+    Game.new(player_1: player_1,
+             player_2: player_2)
+  }
+
+  describe "validations" do
+    it {should validate_presence_of(:player_1)}
+    it {should validate_presence_of(:player_2)}
+  end
+
+  describe "current_turn" do
+    it "can be set to player 1 or player 2" do
+      subject.current_turn = 0
+
+      expect(subject.current_turn).to eq("player_1")
+
+      subject.current_turn = 1
+
+      expect(subject.current_turn).to eq("player_2")
+    end
+  end
 end
